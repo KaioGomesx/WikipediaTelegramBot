4ac80edb0c562d2e9d1f935e8c4b311c41647c47
r1s3 <kaiogomes.nave@gmail.com>
2019-07-02 00:07:23 -0300

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -5,29 +5,40 @@
   Autor: R1S3
   Telegram: @Risezin
   GitHub: https://github.com/RIS33
-  Version: 0.0.1
+  Version: 0.0.2
 */
+
 // API's
 const algorithmia = require('algorithmia');
 const TelegramBot = require('node-telegram-bot-api');
+
 // API's Token
-const algorithmiaToken = 'your algorithmia api token';
-const botToken = 'your telegram bot token';
+const algorithmiaToken: string = 'algorithmia api token here';
+const botToken: string = 'telegram bot token here';
+
 // Bot Init
 const bot = new TelegramBot(botToken, { polling: true });
+
 // Return using mode to user
 bot.onText(/\/start/, (msg, match) => {
     const chatId = msg.chat.id;
     const opts = { reply_to_message_id: msg.message_id };
-    bot.sendMessage(chatId, 'Modo de uso: /wiki TERMO_A_BUSCAR', opts);
+    bot.sendMessage(chatId, 'Hello, friend', opts);
 });
+
+bot.onText(/\/help/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const opts = { reply_to_message_id: msg.message_id};
+  bot.sendMessage(chatId, 'Modo de usar: /wiki idioma termo_a_pesquisar' ,opts)
+});
+
 // Search 
-bot.onText(/\/wiki (.+)/, (msg, match) => {
+bot.onText(/\/wiki (.+) (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
     const opts = { reply_to_message_id: msg.message_id };
     const input = {
-        "articleName": match[1],
-        "lang": 'pt'
+        "articleName": match[2],
+        "lang": match[1]
     };
     algorithmia.client(algorithmiaToken)
         .algo("web/WikipediaParser/0.1.2")
